b5b453a2ac2ed26def42fae996336faf16848048
TuralJafar <76geemk@code.edu.az>
2023-03-28 22:51:20 +0400

    method task

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 6, 10, 2, 11, 23, 14 };
+
+            Min(arr);
+            Console.WriteLine(Min);
+
+
+
+        }
+        public static int Min(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    arr[i] = min;
+                }
+
+
+            }
+
+            Console.WriteLine(min);
+            return min;
+
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 0;
+            Area(3);
+            Area(4, 9);
+            Area(5, 8, 6);
+            Area(4, 5, 6, 7);
+        }
+        public static void Area(int r)
+        {
+
+            decimal pi = 3.14m;
+            Console.WriteLine("cevrenin SAhEsi= ");
+            Console.WriteLine(pi * r * r);
+        }
+        public static void Area(int a, int b)
+        {
+
+            Console.WriteLine("ducbucaqlinin sahesi= ");
+            Console.WriteLine(a * b);
+        }
+        public static void Area(int a, int b, int c)
+        {
+            Console.WriteLine("duzbucaqli paralepidin sahesi= ");
+            Console.WriteLine(2 * (a * b + a * c + b * c));
+        }
+        public static void Area( int a, int b, int c, int r)
+        {
+            
+            int p =  (a + b + c) / 2;
+            Console.WriteLine("3bucagin daxiline cekilen cevrenin sahesi= ");
+            Console.WriteLine(p * r);
+        }
+    }
+}
